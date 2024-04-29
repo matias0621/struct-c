616bf606f0ef19931ce4857362a800363591014c
matias0621 <mattiascorrea@outlook.com>
2024-04-29 11:59:21 -0300

    asd

--- main.c
+++ main.c
@@ -11,6 +11,13 @@ strAlumno cargarAlumnos(strAlumno alumno);
 int cargarArregloDeAlumnos(strAlumno arrAlumnos[], int dim);
 void mostrarAlumnos(strAlumno alumno);
 void mostrarArregloDeAlumnos(strAlumno alumno[], int validos);
+int ingresarMatricula();
+void buscarMatricula(strAlumno alumno[], int validos, int matricula);
+void mostrarAlumnoPorMatricula(strAlumno alumno);
+char ingresarGenero();
+void buscarGenero(strAlumno alumno[], int validos, char genero);
+void mostrarAlumnoPorGenero(strAlumno alumno);
+int cuantosAlumnosDeUnGeneroHay(strAlumno alumn[], int validos, char genero);
 
 int main()
 {
@@ -20,8 +27,14 @@ int main()
 
     int validos = cargarArregloDeAlumnos(arrAlumnos, dim);
 
-    mostrarArregloDeAlumnos(arrAlumnos, validos);
+    //mostrarArregloDeAlumnos(arrAlumnos, validos);
+    //int dato = ingresarMatricula();
+    //buscarMatricula(arrAlumnos, validos, dato);
+    char genero = ingresarGenero();
+    //buscarGenero(arrAlumnos, validos, genero);
+    int sexo = cuantosAlumnosDeUnGeneroHay(arrAlumnos, validos, genero);
 
+    printf("Hay %d de sexo %c", sexo, genero);
     return 0;
 }
 
@@ -84,11 +97,73 @@ int ingresarMatricula(){
     int matricula = 0;
     printf("Ingrese la matricula que desea buscar \n");
     scanf("%d", &matricula);
+    return matricula;
 }
 
-void buscarMatricula(strAlumno alumno[], int validos){
+void buscarMatricula(strAlumno alumno[], int validos, int matricula){
     int i = 0;
     int encontrado = 0;
 
-    while()
+    while(i < validos && encontrado == 0){
+        if (alumno[i].matricula == matricula){
+                mostrarAlumnoPorMatricula(alumno[i]);
+                encontrado = 1;
+        }
+        else{
+            i++;
+        }
+    }
+}
+
+void mostrarAlumnoPorMatricula(strAlumno alumno){
+    printf("Dato del alumno %s: \n", alumno.nombres);
+    printf("/--------------------------------------------------------------/\n");
+    printf("Nombre: %s\n", alumno.nombres);
+    printf("Matricula: %d\n", alumno.matricula);
+    printf("Genero: %c\n", alumno.genero);
+    printf("/--------------------------------------------------------------/\n");
+}
+///Ejercicio 5
+char ingresarGenero(){
+    char genero;
+    printf("Ingrese un genero. M o F\n");
+    fflush(stdin);
+    scanf("%c", &genero);
+    return genero;
+}
+
+void buscarGenero(strAlumno alumno[], int validos, char genero){
+    int i = 0;
+
+    while(i < validos){
+        if (alumno[i].genero == genero){
+                mostrarAlumnoPorGenero(alumno[i]);
+                i++;
+        }
+        else{
+            i++;
+        }
+    }
+}
+
+void mostrarAlumnoPorGenero(strAlumno alumno){
+    printf("/--------------------------------------------------------------/\n");
+    printf("Nombre: %s\n", alumno.nombres);
+    printf("Matricula: %d\n", alumno.matricula);
+    printf("Genero: %c\n", alumno.genero);
+    printf("/--------------------------------------------------------------/\n");
+}
+
+///Ejercicio 8
+int cuantosAlumnosDeUnGeneroHay(strAlumno alumn[], int validos, char genero){
+    int sexo = 0;
+    int i = 0;
+
+    while(i < validos){
+        if (alumn[i].genero = genero){
+            sexo++;
+        }
+        i++;
+    }
+    return sexo;
 }
